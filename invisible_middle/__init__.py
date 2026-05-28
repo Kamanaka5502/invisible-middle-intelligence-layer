00bb08f239a05ace4bfa309ca011c616eea3c39a
Samantha Greenwell Revita <samgreenrevita@gmail.com>
2026-05-27 19:37:00 -0600

    Add package init

---
+++ invisible_middle/__init__.py
@@ -0,0 +1,7 @@
+"""Invisible Middle Intelligence Layer.
+
+Public-safe structural systems intelligence scaffold for hidden human compensation,
+burden transfer, containment instability, and flourishing restoration.
+"""
+
+__version__ = "1.0.0"
